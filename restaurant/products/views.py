e7ce0d9a6877d9eedac22635b04787d50a9a062c
Nicholas Bernardoni <n.bernardoni@proteoeng.com>
2020-10-18 15:40:09 +0200

    Aggiunta delete view

--- restaurant/products/views.py
+++ restaurant/products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse
-from django.views.generic import ListView, DetailView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, DetailView, DeleteView
 from .models import Food
 from .forms import CreateProductForm
 from django.contrib import messages
@@ -32,11 +32,16 @@ def create_food(request):
     return render(request, 'products/food/product_create.html', context)
 
 
-def delete_food(request, id):
-    Food.objects.get(id=id).delete()
-    messages.success(request, 'Prodotto eliminato correttamente!')
-    context = {
-        'messages': messages,
-    }
-    return render(request, 'products/food/product_list.html', context)
+# def delete_food(request, id):
+#     Food.objects.get(id=id).delete()
+#     messages.success(request, 'Prodotto eliminato correttamente!')
+#     context = {
+#         'messages': messages,
+#     }
+#     return render(request, 'products/food/product_list.html', context)
+
+class DeleteProduct(DeleteView):
+    model = Food
+    template_name = 'products/food/product_delete.html'
+    success_url = reverse_lazy('products:product-list')
 
